c1893986ffe0eb6d574b2b17d1c27f9d426d83a8
Malika <118437549+MalikaTarannum@users.noreply.github.com>
2024-02-03 14:16:39 +0500

    Add files via upload

---
+++ react/bulbOnOffJS/js/main.js
@@ -0,0 +1,8 @@
+function bulbOn() {
+    let bulbObj = document.querySelector(".bulbonoff-image");
+    bulbObj.setAttribute("src", "./images/on.png");
+}
+function bulbOff() {
+    let bulbObj = document.querySelector(".bulbonoff-image");
+    bulbObj.setAttribute("src", "./images/of.png");
+}
